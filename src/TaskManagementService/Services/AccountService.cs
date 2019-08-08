5832139737062fcd70a1ede9a810fbd9d0dde0fc
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-08 20:02:42 +0300

    Create AccountService.cs

---
+++ src/TaskManagementService/Services/AccountService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.TaskManagementService.Services
+{
+    public class AccountService
+    {
+        public async Task SignUpAsync(string username, string password, CancellationToken cancellationToken)
+        {
+        }
+    }
+}
