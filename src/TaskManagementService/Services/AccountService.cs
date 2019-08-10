aeb601fd38269972c4a81eba9630912bf7a41419
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-10 15:53:16 +0300

    Update AccountService.cs

--- src/TaskManagementService/Services/AccountService.cs
+++ src/TaskManagementService/Services/AccountService.cs
@@ -1,15 +1,38 @@
 ﻿using IdentityModel.Client;
+using Lionize.TaskManagement.ApiModels;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace TIKSN.Lionize.TaskManagementService.Services
 {
-    public class AccountService
+    public class AccountService : IAccountService
     {
-        public async Task SignInAsync(string username, string password, CancellationToken cancellationToken)
+        public async Task<SignInResponse> SignInAsync(string username, string password, CancellationToken cancellationToken)
         {
-            var discoveryClient = new DiscoveryClient("http://localhost:8081");
-            var discoveryResponse = await discoveryClient.GetAsync(cancellationToken);
+            var client = new HttpClient();
+
+            var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = "http://localhost:8081/connect/token",
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                //Scope = "api1",
+
+                UserName = username,
+                Password = password
+            });
+
+            return new SignInResponse
+            {
+                IsError = response.IsError,
+                Error = response.Error,
+                AccessToken = response.AccessToken,
+                IdentityToken = response.IdentityToken,
+                RefreshToken = response.RefreshToken,
+                TokenType = response.TokenType
+            };
         }
     }
 }
