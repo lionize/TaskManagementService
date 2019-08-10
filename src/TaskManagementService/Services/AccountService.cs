d5a2f48f1398313121a3379a35e7499a6fd9e1be
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-10 15:39:26 +0300

    Update AccountService.cs

--- src/TaskManagementService/Services/AccountService.cs
+++ src/TaskManagementService/Services/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using IdentityModel.Client;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TIKSN.Lionize.TaskManagementService.Services
@@ -7,6 +8,8 @@ namespace TIKSN.Lionize.TaskManagementService.Services
     {
         public async Task SignInAsync(string username, string password, CancellationToken cancellationToken)
         {
+            var discoveryClient = new DiscoveryClient("http://localhost:8081");
+            var discoveryResponse = await discoveryClient.GetAsync(cancellationToken);
         }
     }
 }
