2719d3a1f95c9baf1dad1f16cef04abd1735a6d5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-18 22:36:05 +0300

    Update Startup.cs

--- src/TaskManagementService/Startup.cs
+++ src/TaskManagementService/Startup.cs
@@ -52,6 +52,9 @@ namespace TIKSN.Lionize.TaskManagementService
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.AddApiVersioning();
+            services.AddVersionedApiExplorer();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("1.0", new OpenApiInfo { Title = "Lionize / Task Management Service", Version = "1.0" });
