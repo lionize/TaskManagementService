baadb7c979f5e5dc669d11eca343e9db8d95a485
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-18 22:33:02 +0300

    Create AccountsController.cs

---
+++ src/TaskManagementService/Controllers/V1/AccountsController.cs
@@ -0,0 +1,27 @@
+﻿using Lionize.TaskManagement.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Lionize.TaskManagementService.Services;
+
+namespace TaskManagementService.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService accountService;
+
+        public AccountsController(IAccountService accountService)
+        {
+            this.accountService = accountService ?? throw new ArgumentNullException(nameof(accountService));
+        }
+
+        [HttpPost("SignIn")]
+        public Task<SignInResponse> SignIn([FromBody]SignInRequest request, CancellationToken cancellationToken)
+        {
+            return accountService.SignInAsync(request.Username, request.Password, cancellationToken);
+        }
+    }
+}
