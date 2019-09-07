1a6ba0ec42ba853ea36a57447d176e0927abe226
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-07 17:23:50 +0300

    Create BusinessAutofacModule.cs

---
+++ src/Business/BusinessAutofacModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using MathNet.Numerics.Random;
+using System;
+using TIKSN.Serialization.Numerics;
+
+namespace TIKSN.Lionize.TaskManagementService.Business
+{
+    public class BusinessAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EndpointAddressProvider>()
+                .As<IEndpointAddressProvider>()
+                .SingleInstance();
+
+            builder.RegisterType<CryptoRandomSource>()
+                .As<Random>()
+                .SingleInstance();
+
+            builder.RegisterType<UnsignedBigIntegerBinaryDeserializer>()
+                .SingleInstance();
+        }
+    }
+}
