3b17b472a8ff39e826d6c9eaa4172cdd497df37c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-07 17:23:47 +0300

    Create BusinessMappingProfile.cs

---
+++ src/Business/BusinessMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace TIKSN.Lionize.TaskManagementService.Business
+{
+    public class BusinessMappingProfile : Profile
+    {
+        public BusinessMappingProfile()
+        {
+        }
+    }
+}
