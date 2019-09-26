b2cec54faf82ff3ef11b2f681070e10812eb6104
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-26 14:39:12 +0300

    Delete IEndpointAddressProvider.cs

--- src/Business/IEndpointAddressProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TIKSN.Lionize.TaskManagementService.Business
-{
-    public interface IEndpointAddressProvider
-    {
-        Uri GetEndpointAddress(string queueName);
-    }
-}
