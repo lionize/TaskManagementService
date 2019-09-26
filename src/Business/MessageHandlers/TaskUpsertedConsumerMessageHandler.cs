6ead36ffec832746ecf62089d54c273dc39274ff
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-26 14:57:42 +0300

    Update TaskUpsertedConsumerMessageHandler.cs

--- src/Business/MessageHandlers/TaskUpsertedConsumerMessageHandler.cs
+++ src/Business/MessageHandlers/TaskUpsertedConsumerMessageHandler.cs
@@ -4,11 +4,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using TIKSN.Integration.Correlation;
 using TIKSN.Lionize.Messaging.Handlers;
+using TIKSN.Lionize.TaskManagementService.Data.Repositories;
 
 namespace TIKSN.Lionize.TaskManagementService.Business.MessageHandlers
 {
     public class TaskUpsertedConsumerMessageHandler : IConsumerMessageHandler<TaskUpserted>
     {
+        private readonly IUserTaskRepository _userTaskRepository;
+
+        public TaskUpsertedConsumerMessageHandler(IUserTaskRepository userTaskRepository)
+        {
+            _userTaskRepository = userTaskRepository ?? throw new ArgumentNullException(nameof(userTaskRepository));
+        }
+
         public async Task HandleAsync(TaskUpserted message, CorrelationID correlationID, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
