b8e2fdfbc48bf74ab3f22b8d1f220fa6b6e85ef5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-26 14:48:15 +0300

    Create UserTaskEntity.cs

---
+++ Data/Entities/UserTaskEntity.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using TIKSN.Data;
+
+namespace TIKSN.Lionize.TaskManagementService.Data.Entities
+{
+    public class UserTaskEntity : IEntity<string>
+    {
+        [BsonId]
+        public string ID { get; set; }
+        public Guid UserID { get; set; }
+        public bool Completed { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
