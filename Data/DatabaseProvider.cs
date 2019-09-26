3fadb8e9a03d82923b939ef4033e6680f57bf975
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-26 14:29:40 +0300

    Create DatabaseProvider.cs

---
+++ Data/DatabaseProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using TIKSN.Data.Mongo;
+
+namespace TIKSN.Lionize.TaskManagementService.Data
+{
+    public class DatabaseProvider : MongoDatabaseProvider
+    {
+        public DatabaseProvider(IConfigurationRoot configuration) : base(configuration, "Mongo")
+        {
+        }
+    }
+}
