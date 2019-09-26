331941ed7e69d6a4fa901a94d49c2648300cfa98
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-26 14:39:51 +0300

    Create TaskUpsertedConsumerMessageHandler.cs

---
+++ src/Business/MessageHandlers/TaskUpsertedConsumerMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using Lionize.IntegrationMessages;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Integration.Correlation;
+using TIKSN.Lionize.Messaging.Handlers;
+
+namespace TIKSN.Lionize.TaskManagementService.Business.MessageHandlers
+{
+    public class TaskUpsertedConsumerMessageHandler : IConsumerMessageHandler<TaskUpserted>
+    {
+        public Task HandleAsync(TaskUpserted message, CorrelationID correlationID, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
