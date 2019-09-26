ae0e507435d4aaaf09093666f7710de59d1fbb23
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-26 14:29:36 +0300

    Create DataAutofacModule.cs

---
+++ Data/DataAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using TIKSN.Data.Mongo;
+
+namespace TIKSN.Lionize.TaskManagementService.Data
+{
+    public class DataAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .RegisterType<DatabaseProvider>()
+                .As<IMongoDatabaseProvider>()
+                .SingleInstance();
+        }
+    }
+}
