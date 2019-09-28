2feaa41649cbcdc4fd9b65ef92214cee23250057
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-28 22:27:01 +0300

    Update MatrixController.cs

--- src/TaskManagementService/Controllers/V1/MatrixController.cs
+++ src/TaskManagementService/Controllers/V1/MatrixController.cs
@@ -8,10 +8,16 @@ namespace TIKSN.Lionize.TaskManagementService.Controllers.V1
     [ApiVersion("1.0")]
     [Route("api/{version:apiVersion}/[controller]")]
     [ApiController]
-    public class MatrixController
+    public class MatrixController : ControllerBase
     {
         [HttpGet]
-        public async Task<MatrixTask> Get()
+        public async Task<MatrixTask[]> GetMatrix()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("Backlog")]
+        public async Task<BacklogTask[]> GetBacklog()
         {
             throw new NotImplementedException();
         }
