384adff404f62cfdefcf82c86a921dab603f66a5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-28 22:17:54 +0300

    Create MatrixController.cs

---
+++ src/TaskManagementService/Controllers/V1/MatrixController.cs
@@ -0,0 +1,19 @@
+﻿using Lionize.TaskManagement.ApiModels.V1;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.TaskManagementService.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class MatrixController
+    {
+        [HttpGet]
+        public async Task<MatrixTask> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
