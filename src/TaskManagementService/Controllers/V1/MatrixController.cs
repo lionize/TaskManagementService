b306c57ff5b97287689ce5e37e2cac0c7c5d2ebd
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 18:21:55 +0300

    Update MatrixController.cs

--- src/TaskManagementService/Controllers/V1/MatrixController.cs
+++ src/TaskManagementService/Controllers/V1/MatrixController.cs
@@ -1,25 +1,43 @@
-﻿using Lionize.TaskManagement.ApiModels.V1;
+﻿using AutoMapper;
+using Lionize.TaskManagement.ApiModels.V1;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
+using TIKSN.Lionize.TaskManagementService.Business.Services;
 
 namespace TIKSN.Lionize.TaskManagementService.Controllers.V1
 {
     [ApiVersion("1.0")]
     [Route("api/{version:apiVersion}/[controller]")]
     [ApiController]
+    [Authorize]
     public class MatrixController : ControllerBase
     {
+        private readonly IMatrixTaskService _matrixTaskService;
+        private readonly IMapper _mapper;
+
+        public MatrixController(IMatrixTaskService matrixTaskService, IMapper mapper)
+        {
+            _matrixTaskService = matrixTaskService ?? throw new ArgumentNullException(nameof(matrixTaskService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
         [HttpGet]
-        public async Task<MatrixTask[]> GetMatrix()
+        public async Task<MatrixTask[]> GetMatrix(CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
 
         [HttpGet("Backlog")]
-        public async Task<BacklogTask[]> GetBacklog()
+        public async Task<BacklogTask[]> GetBacklog(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var userId = Guid.Parse(User.FindFirst("sub").Value);
+
+            var entities = await _matrixTaskService.GetBacklogAsync(userId, cancellationToken).ConfigureAwait(false);
+
+            return _mapper.Map<BacklogTask[]>(entities);
         }
     }
 }
