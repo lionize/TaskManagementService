8c12ad575d34442f755816d6d47ed2c0de2ed8e8
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 17:46:53 +0300

    Update IMatrixTaskRepository.cs

--- src/Data/Repositories/IMatrixTaskRepository.cs
+++ src/Data/Repositories/IMatrixTaskRepository.cs
@@ -1,9 +1,13 @@
-﻿using TIKSN.Data.Mongo;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data.Mongo;
 using TIKSN.Lionize.TaskManagementService.Data.Entities;
 
 namespace TIKSN.Lionize.TaskManagementService.Data.Repositories
 {
     public interface IMatrixTaskRepository : IMongoRepository<MatrixTaskEntity, string>
     {
+        Task<int> GetMaxOrderOrDefaultAsync(Guid userID, CancellationToken cancellationToken);
     }
 }
