1b085b08f89140d50b4af7518fed76edbfb76ae8
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 17:33:31 +0300

    Update TaskUpsertedConsumerMessageHandler.cs

--- src/Business/MessageHandlers/TaskUpsertedConsumerMessageHandler.cs
+++ src/Business/MessageHandlers/TaskUpsertedConsumerMessageHandler.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using Lionize.IntegrationMessages;
+using MediatR;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using TIKSN.Integration.Correlation;
 using TIKSN.Lionize.Messaging.Handlers;
+using TIKSN.Lionize.TaskManagementService.Business.Notifications;
 using TIKSN.Lionize.TaskManagementService.Data.Entities;
 using TIKSN.Lionize.TaskManagementService.Data.Repositories;
 
@@ -13,11 +15,13 @@ namespace TIKSN.Lionize.TaskManagementService.Business.MessageHandlers
     public class TaskUpsertedConsumerMessageHandler : IConsumerMessageHandler<TaskUpserted>
     {
         private readonly IUserTaskRepository _userTaskRepository;
+        private readonly IMediator _mediator;
         private readonly IMapper _mapper;
 
-        public TaskUpsertedConsumerMessageHandler(IUserTaskRepository userTaskRepository, IMapper mapper)
+        public TaskUpsertedConsumerMessageHandler(IUserTaskRepository userTaskRepository, IMediator mediator, IMapper mapper)
         {
             _userTaskRepository = userTaskRepository ?? throw new ArgumentNullException(nameof(userTaskRepository));
+            _mediator = mediator;
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
@@ -25,6 +29,7 @@ namespace TIKSN.Lionize.TaskManagementService.Business.MessageHandlers
         {
             var entity = _mapper.Map<UserTaskEntity>(message);
             await _userTaskRepository.AddOrUpdateAsync(entity, cancellationToken).ConfigureAwait(false);
+            await _mediator.Publish(new TaskChangedNotification(entity.ID), cancellationToken).ConfigureAwait(false);
         }
     }
 }
