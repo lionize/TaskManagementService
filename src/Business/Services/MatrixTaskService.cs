a60a094202fba327342209d33332ecc5c262c1d0
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 18:13:20 +0300

    Create MatrixTaskService.cs

---
+++ src/Business/Services/MatrixTaskService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Lionize.TaskManagementService.Data.Entities;
+using TIKSN.Lionize.TaskManagementService.Data.Repositories;
+
+namespace TIKSN.Lionize.TaskManagementService.Business.Services
+{
+    public class MatrixTaskService
+    {
+        private readonly IMatrixTaskRepository _matrixTaskRepository;
+
+        public MatrixTaskService(IMatrixTaskRepository matrixTaskRepository)
+        {
+            _matrixTaskRepository = matrixTaskRepository ?? throw new ArgumentNullException(nameof(matrixTaskRepository));
+        }
+
+        public async Task<MatrixTaskEntity[]> GetBacklogAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            return await _matrixTaskRepository.GetBacklogTasksAsync(userId, cancellationToken);
+        }
+
+        public async Task<MatrixTaskEntity[]> GetActiveAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            return await _matrixTaskRepository.GetActiveAsync(userId, cancellationToken);
+        }
+    }
+}
