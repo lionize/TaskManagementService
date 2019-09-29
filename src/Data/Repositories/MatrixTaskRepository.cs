b651fcc2c1071b4a96a63d6fed0cc65385e224b8
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 17:53:13 +0300

    Update MatrixTaskRepository.cs

--- src/Data/Repositories/MatrixTaskRepository.cs
+++ src/Data/Repositories/MatrixTaskRepository.cs
@@ -1,4 +1,9 @@
-﻿using TIKSN.Data.Mongo;
+﻿using MongoDB.Driver;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data.Mongo;
 using TIKSN.Lionize.TaskManagementService.Data.Entities;
 
 namespace TIKSN.Lionize.TaskManagementService.Data.Repositories
@@ -7,6 +12,20 @@ namespace TIKSN.Lionize.TaskManagementService.Data.Repositories
     {
         public MatrixTaskRepository(IMongoDatabaseProvider mongoDatabaseProvider) : base(mongoDatabaseProvider, "MatrixTasks")
         {
+        }
+
+        public async Task<int> GetMaxOrderOrDefaultAsync(Guid userID, CancellationToken cancellationToken)
+        {
+            var query = await collection.FindAsync(Builders<MatrixTaskEntity>.Filter.Eq(f => f.UserID, userID), cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            var maxOrder = 0;
+
+            while (await query.MoveNextAsync(cancellationToken).ConfigureAwait(false))
+            {
+                maxOrder = Math.Max(maxOrder, query.Current.Max(x => x.Order));
+            }
+
+            return maxOrder;
         }
     }
 }
