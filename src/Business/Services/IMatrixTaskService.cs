d72f5d2a69d281f8db6c9b0c03c635b0daa34e37
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 18:14:29 +0300

    Create IMatrixTaskService.cs

---
+++ src/Business/Services/IMatrixTaskService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Lionize.TaskManagementService.Data.Entities;
+
+namespace TIKSN.Lionize.TaskManagementService.Business.Services
+{
+    public interface IMatrixTaskService
+    {
+        Task<MatrixTaskEntity[]> GetActiveAsync(Guid userId, CancellationToken cancellationToken);
+        Task<MatrixTaskEntity[]> GetBacklogAsync(Guid userId, CancellationToken cancellationToken);
+    }
+}
