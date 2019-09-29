5f35b05d253274be022f907637e01bbc13374bd3
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 18:07:25 +0300

    Update MatrixTaskRepository.cs

--- src/Data/Repositories/MatrixTaskRepository.cs
+++ src/Data/Repositories/MatrixTaskRepository.cs
@@ -16,16 +16,12 @@ namespace TIKSN.Lionize.TaskManagementService.Data.Repositories
 
         public async Task<int> GetMaxOrderOrDefaultAsync(Guid userID, CancellationToken cancellationToken)
         {
-            var query = await collection.FindAsync(Builders<MatrixTaskEntity>.Filter.Eq(f => f.UserID, userID), cancellationToken: cancellationToken).ConfigureAwait(false);
+            var orders = await collection.Find(Builders<MatrixTaskEntity>.Filter.Eq(f => f.UserID, userID)).Project(entity => entity.Order).ToListAsync(cancellationToken).ConfigureAwait(false);
 
-            var maxOrder = 0;
+            if (orders.Count == 0)
+                return default;
 
-            while (await query.MoveNextAsync(cancellationToken).ConfigureAwait(false))
-            {
-                maxOrder = Math.Max(maxOrder, query.Current.Max(x => x.Order));
-            }
-
-            return maxOrder;
+            return orders.Max();
         }
     }
 }
