c0059decb4584b1f965f369351f3bcd2ed491828
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 18:28:05 +0300

    Update MatrixTaskRepository.cs

--- src/Data/Repositories/MatrixTaskRepository.cs
+++ src/Data/Repositories/MatrixTaskRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,12 +15,29 @@ namespace TIKSN.Lionize.TaskManagementService.Data.Repositories
         {
         }
 
+        public Task<IEnumerable<MatrixTaskEntity>> GetActiveAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<MatrixTaskEntity>> GetBacklogTasksAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            return await collection.Find(FilterByUser(userId)).ToListAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private static FilterDefinition<MatrixTaskEntity> FilterByUser(Guid userId)
+        {
+            return Builders<MatrixTaskEntity>.Filter.Eq(f => f.UserID, userId);
+        }
+
         public async Task<int> GetMaxOrderOrDefaultAsync(Guid userID, CancellationToken cancellationToken)
         {
-            var orders = await collection.Find(Builders<MatrixTaskEntity>.Filter.Eq(f => f.UserID, userID)).Project(entity => entity.Order).ToListAsync(cancellationToken).ConfigureAwait(false);
+            var orders = await collection.Find(FilterByUser(userID)).Project(entity => entity.Order).ToListAsync(cancellationToken).ConfigureAwait(false);
 
             if (orders.Count == 0)
+            {
                 return default;
+            }
 
             return orders.Max();
         }
