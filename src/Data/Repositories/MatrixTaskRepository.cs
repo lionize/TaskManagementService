4e350b0537f5941f34918b252b977e027c88cd02
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-29 17:22:53 +0300

    Create MatrixTaskRepository.cs

---
+++ src/Data/Repositories/MatrixTaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using TIKSN.Data.Mongo;
+using TIKSN.Lionize.TaskManagementService.Data.Entities;
+
+namespace TIKSN.Lionize.TaskManagementService.Data.Repositories
+{
+    public class MatrixTaskRepository : MongoRepository<MatrixTaskEntity, string>, IMatrixTaskRepository
+    {
+        public MatrixTaskRepository(IMongoDatabaseProvider mongoDatabaseProvider) : base(mongoDatabaseProvider, "MatrixTasks")
+        {
+        }
+    }
+}
