bdb16964b1449c63e634017dbed1665ec43510e2
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-12 23:35:51 +0300

    Update MatrixHub.cs

--- src/TaskManagementService/Hubs/MatrixHub.cs
+++ src/TaskManagementService/Hubs/MatrixHub.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Lionize.TaskManagement.RealtimeModels;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TIKSN.Lionize.TaskManagementService.Hubs
 {
     public class MatrixHub : Hub<IMatrixHubClient>
     {
+        public Task MoveToMatrix(MoveToMatrixRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MoveToBacklog(MoveToBacklogRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
