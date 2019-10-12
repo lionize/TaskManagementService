4eebfdf7113256b49961f9dabaea41da7e13bc96
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-13 00:18:23 +0300

    Update MatrixTaskOrderingService.cs

--- src/Business/Services/MatrixTaskOrderingService.cs
+++ src/Business/Services/MatrixTaskOrderingService.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using TIKSN.Lionize.TaskManagementService.Data.Repositories;
 
 namespace TIKSN.Lionize.TaskManagementService.Business.Services
 {
     public class MatrixTaskOrderingService : IMatrixTaskOrderingService
     {
+        private readonly IMatrixTaskRepository _matrixTaskRepository;
+
+        public MatrixTaskOrderingService(IMatrixTaskRepository matrixTaskRepository)
+        {
+            this._matrixTaskRepository = matrixTaskRepository ?? throw new ArgumentNullException(nameof(matrixTaskRepository));
+        }
+
         public Task MoveToBacklog(string id, int order, Guid userID, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
