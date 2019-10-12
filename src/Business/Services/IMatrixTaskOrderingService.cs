84241cf87543f3a261a78194947650bfc29922a9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-13 00:08:11 +0300

    Update IMatrixTaskOrderingService.cs

--- src/Business/Services/IMatrixTaskOrderingService.cs
+++ src/Business/Services/IMatrixTaskOrderingService.cs
@@ -1,12 +1,13 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TIKSN.Lionize.TaskManagementService.Business.Services
 {
     public interface IMatrixTaskOrderingService
     {
-        Task MoveToBacklog(string id, int order, CancellationToken cancellationToken);
+        Task MoveToBacklog(string id, int order, Guid userID, CancellationToken cancellationToken);
 
-        Task MoveToMatrix(string id, bool important, bool urgent, int order, CancellationToken cancellationToken);
+        Task MoveToMatrix(string id, bool important, bool urgent, int order, Guid userID, CancellationToken cancellationToken);
     }
 }
