8222a6c83207217f6eed0838fb3a8671c0ff78cc
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-16 15:02:39 +0300

    Update IMatrixHubClient.cs

--- src/TaskManagementService/Hubs/IMatrixHubClient.cs
+++ src/TaskManagementService/Hubs/IMatrixHubClient.cs
@@ -1,6 +1,12 @@
-﻿namespace TIKSN.Lionize.TaskManagementService.Hubs
+﻿using Lionize.TaskManagement.ApiModels.V1;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.TaskManagementService.Hubs
 {
     public interface IMatrixHubClient
     {
+        Task MovedToBacklog(BacklogTask payload);
+
+        Task MovedToMatrix(MatrixTask payload);
     }
 }
